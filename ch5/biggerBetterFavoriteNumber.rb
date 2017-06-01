a6c0cedd908dfafef9cb47c54eead60139684754
jo-koe <koejosephine@gmail.com>
2017-05-31 17:22:17 -0700

    Code for week 3

---
+++ ch5/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts 'What\'s your favorite number?'
+num = gets.to_i
+num = num+1
+puts 'That\'s ok, I guess, but isn\'t '+num.to_s+' just a bit better? :)'
---
+++ ch5/fullNameGreeting.rb
@@ -0,0 +1,8 @@
+puts 'Hello there, and what\'s your first name?'
+name = gets.chomp
+puts 'What\'s your middle name?'
+name = name+' '+gets.chomp
+puts 'What\'s your last name?'
+name = name+' '+gets.chomp
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '. :)'
