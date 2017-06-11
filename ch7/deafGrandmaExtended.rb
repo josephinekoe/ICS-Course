40ac6270157aa95534306c361803a53cc58b26d9
jo-koe <koejosephine@gmail.com>
2017-06-11 12:37:50 -0700

    Fixes deafGrandmaExtended again

--- ch7/deafGrandmaExtended.rb
+++ ch7/deafGrandmaExtended.rb
@@ -1,16 +1,18 @@
-reply = gets.chomp
 counter = 0
-while (reply != 'BYE' || counter != 3)
-	if reply == reply.upcase
-		year = rand(21)+1930
-		puts 'NO, NOT SINCE '+year.to_s+'!'
-	else
-		puts 'HUH?! SPEAK UP, SONNY!'
-	end
+while (counter != 3)
 	reply = gets.chomp
 	if reply == 'BYE'
-		counter = counter+1 
+		counter = counter+1
 	else counter = 0
 	end
+	if reply == reply.upcase		
+		if counter == 3
+			puts 'BYE!'
+		else
+			year = rand(21)+1930
+			puts 'NO, NOT SINCE '+year.to_s+'!'
+		end
+	else 
+		puts 'HUH?! SPEAK UP, SONNY!'
+	end
 end
-puts 'BYE!'
