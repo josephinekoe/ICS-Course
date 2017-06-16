257ec0530c66ecae6fb5ee5ffc5797610d612557
jo-koe <koejosephine@gmail.com>
2017-06-16 01:20:23 -0700

    Fixes newRoman

--- ch9/newRoman.rb
+++ ch9/newRoman.rb
@@ -17,28 +17,46 @@ def roman_numeral num
 	num = num%(10**array[0][0])
 	counter = 0
 	while counter < array.length-1 && num > 0
-		if counter+1 < array.length && num + 10**array[counter+1][0] >= 10**array[counter][0]
-			if counter+2 < array.length && num + 10**array[counter+2][0] >= 10**array[counter][0]
-				if counter+3 < array.length && num + 10**array[counter+3][0] >= 10**array[counter][0]
+		if num >= 0.9*(10**array[counter][0])
+			if num >= 0.99*(10**array[counter][0])
+				if num >= 0.999*(10**array[counter][0])
 					output = output + array[counter+3][1] + array[counter][1]
+					num = num - 0.999*(10**array[counter][0])
 					counter = counter + 3
 				else
 					output = output + array[counter+2][1] + array[counter][1]
+					num = num - 0.99*(10**array[counter][0])
 					counter = counter + 2
 				end
 			else
 				output = output + array[counter+1][1] + array[counter][1]
+				num = num - 0.9*(10**array[counter][0])
 				counter = counter + 1
 			end
-		else
-			if (num%(5*10**array[counter+1][0]))/(10**array[counter+1][0]) == 4
-				output = output+array[counter+1][1]+array[counter+1][2]
+		elsif num >= 0.5*(10**array[counter][0])
+			output = output+array[counter+1][2]
+			num = num-5*(10**array[counter+1][0])
+		elsif num >= 0.4*(10**array[counter][0])
+			if num >= 0.49*(10**array[counter][0])
+				if num >= 0.499*(10**array[counter][0])
+					output = output + array[counter+3][1] + array[counter+1][2]
+					num = num - 0.499*(10**array[counter][0])
+					counter = counter + 3
+				else
+					output = output + array[counter+2][1] + array[counter+1][2]
+					num = num - 0.49*(10**array[counter][0])
+					counter = counter + 2
+				end
 			else
-				output = output + array[counter+1][2]*(num/(5*10**array[counter+1][0])) + array[counter+1][1]*((num%(5*10**array[counter+1][0]))/(10**array[counter+1][0]))
+				output = output + array[counter+1][1] + array[counter+1][2]
+				num = num - 0.4*(10**array[counter][0])
+				counter = counter + 1
 			end
-			counter = counter + 1
+		else 
+			output = output + array[counter+1][1]*(num/(10**array[counter+1][0]))
+			counter = counter+1
+			num = num%(10**array[counter][0])
 		end
-		num = num%(10**array[counter][0])
 	end
 	return output
 end
